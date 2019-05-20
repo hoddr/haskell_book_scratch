30b4a6b777c46641061b91a2f808f9578cd72298
hoddr <hoddr@outlook.com>
2019-05-19 21:46:35 -0400

    More work on ch11

--- ch11/src/Main.hs
+++ ch11/src/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Data.List (sort)
+
 main :: IO ()
 main = undefined
 
@@ -137,3 +139,187 @@ data Garden = Garden Gardener FlowerType
 --   | Rose Gardener
 --   | Lilac Gardener
 --   deriving Show
+
+-- exercise: programmers
+data Sum a b = First a
+             | Second b
+             deriving (Eq, Show)
+
+data Product a b = Product a b deriving (Eq, Show)
+
+data RecordProduct a b =
+  RecordProduct { pfirst :: a
+                , psecond :: b }
+                deriving (Eq, Show)
+
+data OperatingSystem = GnuPlusLinux
+                     | OpenBSDPlusNevermindJustBSDStill
+                     | Mac
+                     | Windows
+                     deriving (Eq, Show)
+
+data ProgLang = Haskell
+              | Agda
+              | Idris
+              | PureScript
+              deriving (Eq, Show)
+
+data Programmer = Programmer
+                { os :: OperatingSystem
+                , lang :: ProgLang }
+                deriving (Eq, Show)
+
+allOperatingSystems :: [OperatingSystem]
+allOperatingSystems =
+  [ GnuPlusLinux
+  , OpenBSDPlusNevermindJustBSDStill
+  , Mac
+  , Windows
+  ]
+
+allLanguages :: [ProgLang]
+allLanguages =
+  [Haskell, Agda, Idris, PureScript]
+
+allProgrammers :: [Programmer]
+allProgrammers = [ Programmer x y | x <- allOperatingSystems, y <- allLanguages ]
+
+-- exponentiation in order
+-- data Quantum = Yes | No | Both deriving (Eq, Show)
+
+-- convert :: Quantum -> Bool
+-- convert = undefined
+
+-- 1.
+-- convert Yes = True
+-- convert No = True
+-- convert Both = True
+
+-- 2.
+-- convert Yes = True
+-- convert No = True
+-- convert Both = False
+
+-- 3.
+-- convert Yes = True
+-- convert No = False
+-- convert Both = True
+
+-- 4.
+-- convert Yes = True
+-- convert No = False
+-- convert Both = False
+
+-- 5.
+-- convert Yes = False
+-- convert No = True
+-- convert Both = True
+
+-- 6.
+-- convert Yes = False
+-- convert No = True
+-- convert Both = False
+
+-- 7.
+-- convert Yes = False
+-- convert No = False
+-- convert Both = True
+
+-- 8.
+-- convert Yes = False
+-- convert No = False
+-- convert Both = False
+
+-- exercises the quad
+-- 1. 4 + 4 = 8
+-- 2. 4 * 4 = 16
+-- 3. 4 ^ 4 = 256
+-- 4. 2 * 2 * 2 = 2 ^ 3 = 8
+-- 5. a -> b -> c = (2 ^ 2) ^ 2 = 8
+-- 6. (4 ^ 4) ^ 2 = 256 ^ 2 = 65536
+--
+-- binary tree section
+data BinaryTree a =
+                  Leaf
+                  | Node (BinaryTree a) a (BinaryTree a)
+                  deriving (Eq, Ord, Show)
+
+insert' :: Ord a => a -> BinaryTree a -> BinaryTree a
+insert' b Leaf = Node Leaf b Leaf
+insert' b (Node left a right)
+  | b == a = Node left a right
+  | b < a = Node (insert' b left) a right
+  | b > a = Node left a (insert' b right)
+
+-- write map for binary tree
+mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
+mapTree _ Leaf = Leaf
+mapTree f (Node left a right) = Node (mapTree f left) (f a) (mapTree f right)
+
+testTree' :: BinaryTree Integer
+testTree' =
+  Node (Node Leaf 3 Leaf)
+  1
+  (Node Leaf 4 Leaf)
+
+mapExpected =
+  Node (Node Leaf 4 Leaf)
+  2
+  (Node Leaf 5 Leaf)
+
+-- acceptance test for mapTree
+mapOkay =
+  if mapTree (+1) testTree' == mapExpected
+  then print "yup okay!"
+  else error "test failed!"
+
+-- binary tree to list
+preorder :: BinaryTree a -> [a]
+preorder Leaf = []
+preorder (Node left a right) = [a] ++ (preorder left) ++ (preorder right)
+
+inorder :: Ord a => BinaryTree a -> [a]
+inorder tree = sort $ preorder tree
+
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node left a right) = (postorder left) ++ (postorder right) ++ [a]
+
+testTree :: BinaryTree Integer
+testTree = Node (Node Leaf 1 Leaf)
+                2
+                (Node Leaf 3 Leaf)
+
+testPreorder :: IO ()
+testPreorder = if preorder testTree == [2, 1, 3]
+                  then putStrLn "Preorder fine!"
+                  else putStrLn "Preorder fails."
+
+testInorder :: IO ()
+testInorder = if inorder testTree == [1,2,3]
+                 then putStrLn "Inorder fine!"
+                 else putStrLn "Preorder fails."
+
+testPostorder :: IO ()
+testPostorder = if postorder testTree == [1,3,2]
+                   then putStrLn "Postorder fine!"
+                   else putStrLn "Postorder fails."
+
+runTests :: IO ()
+runTests = do
+  testPreorder
+  testInorder
+  testPostorder
+
+-- foldr binary tree
+foldTree :: (a -> b -> b) -> b -> BinaryTree a -> b
+foldTree _ acc Leaf = acc
+foldTree f acc tree = foldr f acc $ preorder tree
+
+-- chapter exercises
+-- 1. a
+-- 2. c
+-- 3. b
+-- 4. c
+
+-- vigenere cipher (see ciphers.hs)
---
+++ ch11/src/ciphers.hs
@@ -0,0 +1,38 @@
+module Ciphers where
+
+import Data.Char
+
+-- chr
+-- ord
+-- mod n 26
+-- lower starts at 97
+-- upper start at 65
+caesar :: Int -> String -> String
+caesar n = map (encrypt n)
+  where encrypt :: Int -> Char -> Char
+        encrypt n c
+          | isAlpha c && isLower c = chr $ ((mod ((ord c) + n - (ord 'a'))) 26) + (ord 'a')
+          | isAlpha c && isUpper c = chr $ ((mod ((ord c) + n - (ord 'A'))) 26) + (ord 'A')
+          | otherwise = c
+
+uncaesar :: Int -> String -> String
+uncaesar n = map (decrypt n)
+  where decrypt :: Int -> Char -> Char
+        decrypt n c
+          | isAlpha c && isLower c = chr $ ((mod ((ord c) - n - (ord 'a'))) 26) + (ord 'a')
+          | isAlpha c && isUpper c = chr $ ((mod ((ord c) - n - (ord 'A'))) 26) + (ord 'A')
+          | otherwise = c
+
+newtype Key -> String
+
+-- meet at dawn
+-- ally al lyal
+-- mppr ae oywy
+vigenere :: Key -> String -> String
+vigenere = undefined
+
+unvigenere :: Key -> String -> String
+unvigenere = undefined
+
+mapKeyToString :: Key -> String -> (Char, Char)
+mapKeyToString = undefined
