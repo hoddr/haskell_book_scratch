46b498452631df6e274393b7ec2b0ade71e0008f
hoddr <hoddr@outlook.com>
2019-06-10 12:44:52 -0400

    Some of the test sections

---
+++ ch14/morse/tests/tests.hs
@@ -0,0 +1,91 @@
+module Main where
+
+import qualified Data.Map as M
+import Morse
+import Test.QuickCheck
+import Test.QuickCheck.Gen (oneof)
+
+
+allowedChars :: [Char]
+allowedChars = M.keys letterToMorse
+
+allowedMorse :: [Morse]
+allowedMorse = M.elems letterToMorse
+
+charGen :: Gen Char
+charGen = elements allowedChars
+
+morseGen :: Gen Morse
+morseGen = elements allowedMorse
+
+prop_thereAndBackAgain :: Property
+prop_thereAndBackAgain =
+  forAll charGen
+  (\c -> ((charToMorse c)
+    >>= morseToChar) == Just c)
+
+main :: IO()
+main = quickCheck prop_thereAndBackAgain
+
+-- babby's first arbitrary
+data Trivial = Trivial deriving (Eq, Show)
+
+trivialGen :: Gen Trivial
+trivialGen = return Trivial
+
+instance Arbitrary Trivial where
+  arbitrary = trivialGen
+
+-- identity crisis
+data Identity a =
+                Identity a deriving (Eq, Show)
+
+identityGen :: Arbitrary a => Gen (Identity a)
+identityGen = do
+  a <- arbitrary
+  return (Identity a)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = identityGen
+
+identityGenInt :: Gen (Identity Int)
+identityGenInt = identityGen
+
+-- arbitrary products
+data Pair a b =
+              Pair a b deriving (Eq, Show)
+
+pairGen :: (Arbitrary a,
+            Arbitrary b) =>
+            Gen (Pair a b)
+pairGen = do
+  a <- arbitrary
+  b <- arbitrary
+  return (Pair a b)
+
+instance (Arbitrary a,
+          Arbitrary b) =>
+          Arbitrary (Pair a b) where
+  arbitrary = pairGen
+
+pairGenIntString :: Gen (Pair Int String)
+pairGenIntString = pairGen
+
+-- sum types
+data Sum a b =
+               First a
+             | Second b
+             deriving (Eq, Show)
+
+-- equal odds
+sumGenEqual :: (Arbitrary a,
+                Arbitrary b) =>
+                Gen (Sum a b)
+sumGenEqual = do
+  a <- arbitrary
+  b <- arbitrary
+  oneof [return $ First a,
+         return $ Second b]
+
+sumGenCharInt :: Gen (Sum Char Int)
+sumGenCharInt = sumGenEqual
