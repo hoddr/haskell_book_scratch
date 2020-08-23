386bca5b2642375eef003eb40027c2617714563b
hoddr <hoddr@outlook.com>
2020-08-23 18:33:41 -0400

    10.6 section exercises

--- ch10/Main.hs
+++ ch10/Main.hs
@@ -1,4 +1,45 @@
 module Main where
 
+import Data.Time
+
+data DatabaseItem = DbString String
+                  | DbNumber Integer
+                  | DbDate UTCTime
+                  deriving (Eq, Ord, Show)
+
+theDatabase :: [DatabaseItem]
+theDatabase =
+  [ DbDate (UTCTime
+           (fromGregorian 1911 5 1)
+           (secondsToDiffTime 34123))
+  , DbNumber 9001
+  , DbString "Hello, world!"
+  , DbDate (UTCTime
+           (fromGregorian 1921 5 1)
+           (secondsToDiffTime 34123))
+  ]
+
+filterDbDate :: [DatabaseItem] -> [UTCTime]
+filterDbDate = foldr (\next acc ->
+                      case next of
+                           (DbDate utc) -> utc : acc
+                           _ -> acc) []
+
+filterDbNumber :: [DatabaseItem] -> [Integer]
+filterDbNumber = foldr (\next acc -> case next of
+                       (DbNumber i) -> i : acc
+                       _ -> acc) []
+
+
+mostRecent :: [DatabaseItem] -> UTCTime
+mostRecent = maximum . filterDbDate
+
+sumDb :: [DatabaseItem] -> Integer
+sumDb = sum . filterDbNumber
+
+avgDb :: [DatabaseItem] -> Double
+avgDb dbs = sum xs / (fromIntegral $ length xs)
+  where xs = map fromIntegral $ filterDbNumber dbs
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = undefined
