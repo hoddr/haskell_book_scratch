25894afac2b3451854f37ededb06acb9f078ab81
hoddr <hoddr@outlook.com>
2020-08-23 20:58:29 -0400

    close but last two funcs are not hitting compare test case

--- ch10/Main.hs
+++ ch10/Main.hs
@@ -41,5 +41,80 @@ avgDb :: [DatabaseItem] -> Double
 avgDb dbs = sum xs / (fromIntegral $ length xs)
   where xs = map fromIntegral $ filterDbNumber dbs
 
+
+-- ch 10 exercises
+stops :: [Char]
+stops = "pbtdkg"
+
+vowels :: [Char]
+vowels = "aeiou"
+
+stopVowelStop :: [Char] -> [Char] -> [(Char, Char, Char)]
+stopVowelStop ss vws = [(x, y, z) | x <- ss, y <- vws, z <- ss]
+
+onlyPs :: [(Char, Char, Char)] -> [(Char, Char, Char)]
+onlyPs = filter (\(x, y, z)-> x == 'p')
+
+-- 2
+-- divides number of chars by number of words
+seekritFunc :: [Char] -> Int
+seekritFunc x = div (sum (map length (words x)))
+                    (length (words x))
+
+betterFunc :: [Char] -> Double
+betterFunc x = (/) (sum (map (fromIntegral . length) (words x)))
+                   ((fromIntegral . length) (words x))
+
+-- rewriting as folds
+-- point-free if possible
+-- myOr
+myOr :: [Bool] -> Bool
+myOr = foldr (||) False
+
+--foldr :: Foldable t => (a -> b -> b) -> b -> t a -> b
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny pred = foldr (\n a -> a || (pred n)) False
+
+-- via fold
+myElem :: Eq a => a -> [a] -> Bool
+myElem test = foldr (\n acc -> acc || (test == n)) False
+
+-- via any
+-- any :: Foldable t => (a -> Bool) -> t a -> Bool
+myElem' :: Eq a => a -> [a] -> Bool
+myElem' test = any (\n -> test == n)
+
+myReverse :: [a] -> [a]
+myReverse = foldl (flip (:)) []
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f = foldr (\n acc -> f n : acc) []
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter pred = foldr (\n acc -> if pred n
+                      then n : acc
+                      else acc) []
+
+squish :: [[a]] -> [a]
+squish = foldr (++) []
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap f = foldr (\n acc -> f n ++ acc) []
+
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap id
+
+-- these are weird...
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy f (x:xs) = foldl (\acc n -> case f n acc of
+                      GT -> acc
+                      _ -> n) x xs
+
+-- these are weird...
+myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+myMinimumBy f (x:xs) = foldl (\acc n -> case f n acc of
+                                             LT -> acc
+                                             _ -> n) x xs
+
 main :: IO ()
 main = undefined
